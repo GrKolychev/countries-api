3f3345b00608a1a2b58f10823021758f1a2f96ff
GrKolychev <kolychevgv92@gmail.com>
2025-04-10 23:45:19 +0800

    #1 prepare basic project skeleton

---
+++ src/test/kotlin/com/countries_counter/couintries_api/CountriesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.countries_counter.couintries_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CountriesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
